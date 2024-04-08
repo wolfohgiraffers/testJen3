591872f93d1f934aabe8978d35f8c2642c041b91
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2024-04-08 14:31:59 +0900

    reconnect jenkins

--- src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
+++ src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
@@ -29,7 +29,7 @@ class CalculatorServiceTests {
     }
 
     @DisplayName("두 수의 합 구하기 테스트")
-    @ParameterizedTest  
+    @ParameterizedTest
     @MethodSource("provideDTOSource")
     void testPlus(CalculatorDTO input) {
 
